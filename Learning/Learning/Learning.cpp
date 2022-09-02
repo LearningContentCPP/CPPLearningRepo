59472c4a2788416ef3af0342a482f9baaaddf762
Xinnan Shen <xinnan.shen@outlook.com.au>
2022-09-02 15:37:02 +0800

    hello world code
    for testing software installation

---
+++ Learning/Learning/Learning.cpp
@@ -0,0 +1,12 @@
+﻿// Learning.cpp : Defines the entry point for the application.
+//
+
+#include "Learning.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
